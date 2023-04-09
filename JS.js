d6ba2653c6701c792727f6b899d90076737e255a
Marco <marco.seraglini@stud.unifi.it>
2023-04-09 19:40:37 +0200

    Aggiornamenti vari giorno di Pasqua.

---
+++ JS.js
@@ -0,0 +1,6 @@
+const lenteingrandimento = document.getElementById('lenteingrandimento');
+const barraRicerca = document.getElementById('barra-ricerca');
+
+lenteingrandimento.addEventListener('click', () => {
+    barraRicerca.classList.toggle('hidden');
+});
