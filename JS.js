c9a8dc1fccfbddf33059a010caae0908058ba763
Marco <marco.seraglini@stud.unifi.it>
2023-04-13 09:10:38 +0200

    Aggiornamenti vari.

--- JS.js
+++ JS.js
@@ -1,6 +1,48 @@
-const lenteingrandimento = document.getElementById('lenteingrandimento');
-const barraRicerca = document.getElementById('barra-ricerca');
 
-lenteingrandimento.addEventListener('click', () => {
-    barraRicerca.classList.toggle('hidden');
+//Menu a tendina
+let button = document.getElementById("menutendina");
+let menutendinaopen = document.getElementById("menutendinaopen");
+let pressed = false;
+
+button.addEventListener("click",aperturamenu);
+
+function aperturamenu(){
+    if (!pressed){
+        pressed=true;
+        menutendinaopen.style.display="list-item";
+        // Disabilita lo scorrimento della pagina
+        document.body.style.overflow = "hidden";
+    }
+    else{
+        pressed=false;
+        menutendinaopen.style.display="none";
+        // Abilita lo scorrimento della pagina
+        document.body.style.overflow = "auto";
+    }
+}
+
+
+
+
+
+
+
+//Header
+var lastScroll = 0;
+var header = document.getElementById("header");
+
+window.addEventListener("scroll", function() {
+    var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScroll > lastScroll) {
+        // scorrimento verso il basso
+        header.classList.remove("visible");
+        header.classList.add("hidden");
+    } else {
+        // scorrimento verso l'alto
+        header.classList.remove("hidden");
+        header.classList.add("visible");
+    }
+    lastScroll = currentScroll;
 });
+
+
